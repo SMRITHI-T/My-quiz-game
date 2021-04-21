2197b667dd415b732fad7fc6ed83de05310886e9
SMRITHI-T <74653719+SMRITHI-T@users.noreply.github.com>
2021-04-21 15:28:12 +0530

    Add files via upload

--- Quiz.js
+++ Quiz.js
@@ -30,15 +30,9 @@ class Quiz {
 
   play(){
     //write code here to hide question elements
-    this.question.hide();
-    this.option1.hide();
-    this.option2.hide();
-    this.option3.hide();
-    this.option4.hide();
-    this.title.hide();
-    this.input1.hide();
-    this.input2.hide();
-    this.button.hide();
+   question.hide();
+
+    
 
 
     //write code to change the background color here
@@ -47,28 +41,30 @@ background("turquoise blue")
 textSize(35);
     text("RESULTS",400,100);
     //call getContestantInfo( ) here
-if(allContestants!==undefined){
+    Contestant.getPlayerInfo();
 
-}
 
     //write condition to check if contestantInfor is not undefined
-
+    if(allContestants!==undefined){
+      var answers=400;
+      fill("pink");
+      textSize(15);
+      text("*NOTE: Contestants who answered correct are highlighted in green colour",100,300);
+      for(var player in allContestants){
+        var correctAns="2";
+        if(correctAns===allContestants[player].answer){
+        fill("green")
+            }else{
+            fill("red");
+          }
+          answers=answers+30;
+          textSize(50);
+          text(allContestants[player].name+":"+allContestants[player].answer,250,answer)
+            }
+      }
     //write code to add a note here
-    fill(pink);
-    textSize(15);
-    text("*NOTE: Contestants who answered correct are highlighted in green colour")
+  
     //write code to highlight contest who answered correctly
-    for(var player in allContestants){
-var correctAns="2";
-if(correctAns===allContestants[player].answer)
-fill("green")
-    }else{
-    fill("red");
-  }
-
+ 
 }
-
-//static getPlayerInfo(){
-  //var playerInfoR=database.ref('players')
-//playerInfoR.on('value',(data)=>{
-  //allPlayers=data.val()
+}
